780959cb90354013fca5e17f866e6da925aaa05d
DarrenRR <56203057+DarrenRR@users.noreply.github.com>
2021-02-13 23:36:31 -0400

    Query DB

---
+++ js/AddTrip.js
@@ -0,0 +1,39 @@
+// ====================== D  R  I  V  E  R  S DATABASE INFORMATION ==========================
+let db=firebase.firestore();
+            // ------------ ADD DATA TO DB (works with forms only!) -----------------------
+driverAddBtn.addEventListener('click', (e) =>{
+    e.preventDefault();
+    db.collection("Trips").doc(tempDemail).set({
+        FirstName: tempDfname,
+        LastName: tempDlname, 
+        Email: tempDemail, 
+        Cell: tempDcell, 
+        AvailableSeats: tempDseats, 
+        Price: tempDprice, 
+        DriverLicense: tempDlicense,
+        AdditionalNotes: tempDdescription,
+    });
+}); //adds data using the button
+            
+            // ------------ EDIT DATA SPECIFIED DB (works with forms only!)-----------------------
+driverEditBtn.addEventListener('click', (e) =>{
+    e.preventDefault();
+    const temp1={
+        FirstName: tempDfname,
+        LastName: tempDlname, 
+        Cell: tempDcell, 
+        AvailableSeats: tempDseats, 
+        Price: tempDprice, 
+        DriverLicense: tempDlicense,
+        AdditionalNotes: tempDdescription
+    };
+    db.collection("Trips").doc(tempDemail).update(temp1);
+}); //edits data using the button 
+            
+            
+            // ------------ DELETES DATA FROM DB (works with forms only!)-----------------------
+            
+driverDelBtn.addEventListener('click', (e) =>{
+    e.preventDefault();
+    db.collection("Trips").doc(tempDemail).delete();
+}); //deletes data using the button
---
+++ js/search.js
@@ -0,0 +1,20 @@
+//Retrieve all carpools based on the user input
+async function getTrips(start_location, stop_location){
+    var startLocation = document.getElementById("start_location").value;
+    var stopLocation = document.getElementById("stop_location").value;
+    console.log(startLocation);
+    var carpools = firebase.firestore().collection("Trips").where("Destination", "==", stopLocation).where("Start", "==", startLocation).get();
+    //var bookings=firebase.firestore().collection("bookings").where("tutor","==",user).get();  //firebase query function
+    var resolved;
+    carpools.catch(function(error){
+        console.log(error);
+        resolved=false;
+    });
+    carpools=await carpools;
+    if(resolved==false)
+      return null;
+    carpools.forEach(function(result){
+        console.log(result.data().Price)
+    });
+    return carpools;
+}
